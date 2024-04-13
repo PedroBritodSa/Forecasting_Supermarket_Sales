58333d5c0f7520ea263f1ac2f3906fa2aa38a3a0
Pedro de Sá <135625404+PedroBritodSa@users.noreply.github.com>
2024-04-13 10:42:55 -0300

    Simplifying code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from prediction import predict
-import altair as alt
+#import altair as alt
 
 
 st.title("Supermarket Sales Forecasting")
@@ -23,12 +23,11 @@ if st.button("Predict Sales"):
     result = predict(Number_of_months)
     st.text(result)
 
-    df_forecast = pd.DataFrame({'Date': pd.date_range(start='2023-08-01', periods=Number_of_months, freq='MS'),
-                                'Forecasted Sales': result})
+    #df_forecast = pd.DataFrame({'Date': pd.date_range(start='2023-08-01', periods=Number_of_months, freq='MS'), 'Forecasted Sales': result})
     
-    chart = alt.Chart(df_forecast).mark_line().encode(
-        x='Date',
-        y=alt.Y('Forecasted Sales', scale=alt.Scale(domain=(90, 125))),
-    ).properties(width=600, height=400)
+    #chart = alt.Chart(df_forecast).mark_line().encode(
+    #    x='Date',
+    #    y=alt.Y('Forecasted Sales', scale=alt.Scale(domain=(90, 125))),
+    #).properties(width=600, height=400)
 
-    st.altair_chart(chart)
+    #st.altair_chart(chart)
